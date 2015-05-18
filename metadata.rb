7ceb4f4586736e6e4f7b66500dd942ec5aa7228b
Larry Zarou <lzarou@commercehub.com>
2015-05-18 15:39:38 -0400

    setting jmxtrans_opts to be configurable and exported into env with service

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "gs.biju at gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures jmxtrans"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.0"
+version          "1.2.0"
 
 depends "ark"
